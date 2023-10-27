addfdbcda8433c39d6e9eb8282996f5afe68798c
Rahul Sarode <rahulsarode048@gmail.com>
2023-10-27 23:40:51 +0530

    DOM Overview - Walking with DOM

---
+++ 3. JavaScript/JavaScript In Browser/31. Walking The DOM/script.js
@@ -0,0 +1,8 @@
+//Changing the ul last element
+document.querySelector("ul").lastElementChild.innerHTML = "Rahul Sarode"
+
+//Changing H1 color to red
+document.firstElementChild.lastElementChild.firstElementChild.style.color = "red"
+
+// getting element by class name
+document.getElementsByClassName("my-list-item")[1].style.color = "blue"
