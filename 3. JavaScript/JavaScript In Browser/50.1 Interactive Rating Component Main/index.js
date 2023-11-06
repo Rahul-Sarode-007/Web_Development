ac0ee76f63e4a711b1c84e45352b75d07a3b88ea
Rahul Sarode <rahulsarode048@gmail.com>
2023-11-06 23:31:53 +0530

    Created rating card after submiting (Thank You) screen from scratch

--- 3. JavaScript/JavaScript In Browser/50.1 Interactive Rating Component Main/index.js
+++ 3. JavaScript/JavaScript In Browser/50.1 Interactive Rating Component Main/index.js
@@ -1,25 +1,61 @@
+//Rating Click
+
 let ratingBoxes = document.querySelectorAll(".rating-digits div");
+let ratingDigit
+
 
 ratingBoxes.forEach((ratingBox, index, array) => {
   const selectedRatingBox = (e) => {
     ratingBoxes.forEach((resetColor) => {
       resetColor.style.background = "#262f38";
+      resetColor.style.color = "#757c87";
+
     });
 
-    console.log(e.target.innerHTML);
+    // console.log(e.target.innerHTML);
     ratingBox.style.background = "grey";
     ratingBox.style.color = "white";
+    ratingDigit = e.target.innerHTML
+
+    //Back-Side
+    let ratingMessage = document.querySelector(".back-side-given-rating p")
+    ratingMessage.innerHTML = `You selected ${ratingDigit} out of 5`
   };
 
   const addHoverClass = () => {
     ratingBox.classList.add("selected");
-    console.log("hi");
   };
 
   ratingBox.addEventListener("mouseover", addHoverClass);
   ratingBox.addEventListener("click", selectedRatingBox);
 });
 
+
+
+
+
+// ***************************************************************
+
+
+//Button Click
+
+let ratingSubmitButton = document.querySelector(".front-side-button")
+
+const submitRating = () =>{
+  let frontSide = document.querySelector(".front-side-container")
+  frontSide.style.zIndex = "-1"
+}
+
+ratingSubmitButton.addEventListener("click", submitRating)
+
+
+
+
+
+
+
+
+// ************************************************************
 
 
 
@@ -42,3 +78,4 @@ ratingBoxes.forEach((ratingBox, index, array) => {
 //     }
 
 // }
+
