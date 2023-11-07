d226fe2dc186f3f5e1b34c255c42162585c12c25
Rahul Sarode <rahulsarode048@gmail.com>
2023-11-07 23:58:40 +0530

    Created Digital Clock

---
+++ 3. JavaScript/JavaScript In Browser/51. Digital Clock With HTML CSS JS/script.js
@@ -0,0 +1,31 @@
+let clock = document.querySelector(".container h1")
+const digitalClock = () =>{
+    let a = new Date()
+    let h = a.getHours()
+    let m = a.getMinutes()
+    let s = a.getSeconds()
+    // console.log(h,m,s)
+
+    if(s<=9){
+        s = "0"+s
+    }
+
+    if(m<=9){
+        m = "0"+m
+    }
+
+    if(h<=9){
+        h = "0"+h
+    }
+
+    if (h>12){
+        h  = h -12
+     }
+
+    clock.innerHTML = `${h}:${m}:${s} ${"PM"} `
+}
+
+setInterval(digitalClock,1000)
+
+
+document.querySelector("h1").style.color = "red"
