a7eb0ffecd0c6163140253a0d9bc0940990e4253
Rahul Sarode <rahulsarode048@gmail.com>
2023-11-21 23:30:22 +0530

    Tip Calculator HTML - CSS - JS  done, Final touch remaining

--- 3. JavaScript/JavaScript In Browser/63.1. Tip Calculator App/script.js
+++ 3. JavaScript/JavaScript In Browser/63.1. Tip Calculator App/script.js
@@ -1,49 +1,46 @@
-let tolAmtInput = document.querySelector(".card1-div1 input")
-let tipArr =  document.querySelectorAll("div.card1-div2 input")
-let numPeople = document.querySelector(".card1-div3 input")
+let tolAmtInput = document.querySelector(".card1-div1 input");
+let numPeopleInput = document.querySelector(".card1-div3 input");
+let selectedTipAmt = 0;
+
+tolAmtInput.addEventListener("input", function () {
+  calculatePerPerson();
+});
+
+numPeopleInput.addEventListener("input", function () {
+  calculatePerPerson();
+});
+
+let tipArr = document.querySelectorAll("div.card1-div2 input");
+tipArr.forEach((element) => {
+  element.addEventListener("click", () => {
+    tipArr.forEach((oldElement) => {
+      oldElement.style.background = "hsl(183, 100%, 15%)";
+    });
+    tipArr[5].style.background = "hsl(189, 41%, 97%)";
+    element.style.background = "hsl(172, 67%, 45%)";
+    selectedTipAmt = element.value;
+    selectedTipAmt = selectedTipAmt.replace("%", "");
+
+    selectedTipAmt = Number.parseInt(selectedTipAmt);
+
+    calculatePerPerson();
+  });
+});
+
+const calculatePerPerson = () => {
+  let tolAmtValue = Number.parseFloat(tolAmtInput.value);
+  let numPeopleValue = Number.parseFloat(numPeopleInput.value);
+
+  let perPerson =
+    (tolAmtValue + (selectedTipAmt / 100) * tolAmtValue) / numPeopleValue;
+  let perPersonTip = ((selectedTipAmt / 100) * tolAmtValue) / numPeopleValue;
+
+  if (!isNaN(perPerson)) {
+    document.querySelector(".tip-amount-person-span").innerHTML =
+      "$" + perPerson.toFixed(2);
+      document.querySelector(".tip-amount-dollor-span").innerHTML =
+        "$" + perPersonTip.toFixed(2);
+  }
 
 
-const cal = async() =>{
-    let p1 = new Promise((resolve)=>{
-        return tolAmtInput.addEventListener("input", ()=>{
-            let tolAmtValue = tolAmtInput.value
-            tolAmtValue = Number.parseInt(tolAmtValue)
-            confirm.log(tolAmtValue)
-            resolve(tolAmtValue)
-        })
-    })
-
-    
-    
-    let p2 = new Promise((resolve)=>{
-        return tipArr.forEach(element => {
-            element.addEventListener("click", ()=>{
-                let selectedTipAmt = element.value
-                selectedTipAmt = selectedTipAmt.replace('%', '')
-                selectedTipAmt = Number.parseInt(selectedTipAmt)
-                console.log(selectedTipAmt)
-                resolve(selectedTipAmt)
-            })
-        });
-    })
-    
-    
-    let p3 = new Promise((resolve)=>{
-      return numPeople.addEventListener("input", ()=>{
-        let numPeopleValue = numPeople.value
-        numPeopleValue = Number.parseInt(numPeopleValue)
-        console.log(numPeopleValue)  
-        resolve(numPeopleValue)
-    })
-    })
-    
-    
-}
-
-
-
-// let perPersonTotalAmount = tolAmtValue+(selectedTipAmt/100*tolAmtInput)
-
-// let perPersonTotalAmountSelector = document.querySelector(".tip-amount-person-span")
-
-
+};
