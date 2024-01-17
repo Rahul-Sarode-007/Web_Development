e8a2cb02750a11a98abf37746b3be2340adf1ce4
Rahul Sarode <rahulsarode048@gmail.com>
2024-01-17 21:48:41 +0530

    learned Local And Global Scope

---
+++ 3. JavaScript/JavaScript In Browser/88. Scope local and global/script.js
@@ -0,0 +1,39 @@
+//Global Scope
+
+let c = 5
+
+{    console.log(c)
+
+}
+
+console.log(c)
+
+const myFun2 = () =>{
+    console.log(c)
+}
+
+myFun2()
+
+// ************************
+
+//Local Scope
+
+// block level scope
+{
+    let a = 8
+}
+console.log(a) //OUTPUT: Uncaught ReferenceError: a is not defined
+// Note: 'a' has the block level scope here
+
+
+
+
+// Functional level scope
+const myFun = () =>{
+    let b = 10
+}
+
+console.log(b)  //Uncaught ReferenceError: b is not defined
+// Note: 'b' has the functional level scope here
+
+myFun()
