895e33714d19e56de4bc6a566bf3b90a478fda41
Rahul Sarode <rahulsarode048@gmail.com>
2024-01-26 22:46:14 +0530

    Keeper App - Creating notes compenemnt dyanamic  from array using props

--- 4. React/3. Enviroment Setup For React/my-app/src/components/App.jsx
+++ 4. React/3. Enviroment Setup For React/my-app/src/components/App.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import Header from "./Header"
-import Note from "./Note"
-import Footer from "./Footer"
+import React from "react";
+import Header from "./Header";
+import Note from "./Note";
+import Footer from "./Footer";
+import notes from "../notes";
 
 export default function App() {
-  return <>
-    <Header />
-    <Note />
-    <Footer />
-    </>
+  return (
+    <div>
+      <Header />
+      {notes.map((noteItem) => {
+        return (
+          <Note
+            key={noteItem.key}
+            title={noteItem.title}
+            content={noteItem.content}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
 }
