78208374bc8f68c97938903e026a9852fd8bbeb8
Rahul Sarode <rahulsarode048@gmail.com>
2024-02-06 12:45:23 +0530

    Practicing spread operator in react

--- 4. React/13.1 Spread Operator Practice/src/components/App.jsx
+++ 4. React/13.1 Spread Operator Practice/src/components/App.jsx
@@ -1,25 +1,18 @@
 import React, { useState } from "react";
 
-function App() {
-  let value = ""
+export default function App() {
+  let [value, setValue] = useState("");
+  let [items, setItems] = useState([]);
 
+  const handleChange = (event) => {
+    value = event.target.value;
+    setValue(value);
+  };
 
-  const [item, setItem] = useState("")
-  const [myItems, setMyItems] = useState(["add Items"])
-
-  const handleChange = (event) =>{
-    value = event.target.value
-    console.log(value)
-  }
-
-
-  const handleClick = () =>{
-    myItems.push(value)
-    console.log(myItems);
-    setItem(...myItems)
-  }
-
-
+  const handleClick = () => {
+    setItems((prevValue) => [...prevValue, value]);
+    setValue("");
+  };
 
   return (
     <div className="container">
@@ -27,25 +20,24 @@ function App() {
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" 
+        <input
+          value={value}
+          name="itemInput"
+          type="text"
           onChange={handleChange}
         />
+
         <button onClick={handleClick}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-        {myItems.map((item)=>{
-          console.log(item)
-          return <li>{item}</li>
-        })}
+          {items.map((item, index) => {
+            return <li key={index}>{item}</li>;
+          })}
         </ul>
-        
-
       </div>
     </div>
   );
 }
-
-export default App;
