9849a41e7102f14313b9a14ce7bf616b573dc030
Rahul Sarode <rahulsarode048@gmail.com>
2024-03-22 23:54:30 +0530

    Learned how JSX woks under the hood

--- 4. React/0. Chai Aur React/Vite Bundler Setup/01-vite-react-setup/src/main.jsx
+++ 4. React/0. Chai Aur React/Vite Bundler Setup/01-vite-react-setup/src/main.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+
+//Directly storing HTML in varivale
+const anotherElement = (
+  <>
+    <a href="https://www.google.com" target="_black">
+      Visit Google
+    </a>
+    <h1>ABC</h1>
+  </>
+);
+
+
+//This is how react create object form JSX
+const reactElement = React.createElement(
+  "a",
+  { href: "https://www.google.com", target: "_blank" },
+  "Please Visit Google"
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // anotherElement
+  reactElement
+);
