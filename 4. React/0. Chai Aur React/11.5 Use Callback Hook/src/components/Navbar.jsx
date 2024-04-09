ea250617837422cbc386442a1c633729edffda5b
Rahul Sarode <rahulsarode048@gmail.com>
2024-04-09 23:06:19 +0530

    Learning useCallback() Hook

---
+++ 4. React/0. Chai Aur React/11.5 Use Callback Hook/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { memo } from 'react'
+
+function Navbar({ajective,getAjective}) {
+    console.log("Navbar is rendering...")
+  return (
+    <>
+    <div>I am a {ajective} Navbar</div>
+    <button onClick={()=>{getAjective()}}>button</button>
+    </>
+  )
+}
+
+export default memo(Navbar)
