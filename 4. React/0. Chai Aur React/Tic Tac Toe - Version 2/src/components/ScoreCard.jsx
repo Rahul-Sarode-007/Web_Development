77e07ee62c24ecc01621a06bfe5ae1325c19f506
Rahul Sarode <rahulsarode048@gmail.com>
2024-08-05 22:02:24 +0530

    tic-tac-toe game - improved CSS

--- 4. React/0. Chai Aur React/Tic Tac Toe - Version 2/src/components/ScoreCard.jsx
+++ 4. React/0. Chai Aur React/Tic Tac Toe - Version 2/src/components/ScoreCard.jsx
@@ -1,18 +1,45 @@
 import React from "react";
 
-export default function ScoreCard({ pin, player, appWinnerSign, score}) {
-  console.log("score1: ", score)
-  console.log("score2: ", score)
+export default function ScoreCard({ pin, player, appWinnerSign, score }) {
+  console.log("score1: ", score);
+  console.log("score2: ", score);
 
+  const signXStyle = {
+    color: "#f44336",
+    fontWeight: "900",
+  };
+
+  const signOStyle = {
+    color: "#2196f3",
+    fontWeight: "700",
+  };
+
+  const getSignStyle = () => {
+    if ((pin === "yes" && appWinnerSign === "X") || (pin !== "yes" && appWinnerSign === "O")) {
+      return signXStyle;
+    } else {
+      return signOStyle;
+    }
+  };
+
+  const getSign = () => {
+    if (pin === "yes") {
+      return appWinnerSign === "X" ? "X" : "O";
+    } else {
+      return appWinnerSign === "O" ? "X" : "O";
+    }
+  };
 
   return (
     <div className="scorecard-container">
       <div className="player-card">
-      <div className="player-name">{player || "Player1"}</div>
-      <div className={`player-sign`}>
-      {pin==="yes"?(appWinnerSign === "X" ? "X" : "O"): (appWinnerSign === "O" ? "X" : "O")}
-      </div>
-      <div className="player-score">{score}</div>
+        <div>
+          <div className="player-name">{player || "Player1"}</div>
+          <div className="player-sign" style={getSignStyle()}>
+            {getSign()}
+          </div>
+          <div className="player-score">{score}</div>
+        </div>
       </div>
     </div>
   );
